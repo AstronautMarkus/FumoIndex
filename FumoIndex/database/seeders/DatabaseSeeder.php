8ac7e5e8caeb018a54053b98d0a400e5ae1e90a6
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-14 22:20:55 -0300

    feat: add CharacterSeeder to populate characters table with predefined characters

--- FumoIndex/database/seeders/DatabaseSeeder.php
+++ FumoIndex/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             FranchiseSeeder::class,
             FumoTypeSeeder::class,
+            CharacterSeeder::class,
         ]);
     }
     
