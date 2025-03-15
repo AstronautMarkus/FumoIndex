68da6c8c0837687608541ebd0797e1de68f84e66
Fernando Catalan <afk656902@hotmail.com>
2025-03-14 21:07:32 -0300

    React app Setup
    React app setup on laravel

---
+++ FumoIndex/resources/app/routes.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <>
+        </>,
+    }
+]);
+
+export default function Router() {
+    return <RouterProvider router={router} />
+};
