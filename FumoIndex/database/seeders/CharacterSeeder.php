93817d3b6a065fe5a4729be9f3def21e346aaf5a
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-15 16:17:57 -0300

    feat: add character data JSON file and update CharacterSeeder to load characters from the file

--- FumoIndex/database/seeders/CharacterSeeder.php
+++ FumoIndex/database/seeders/CharacterSeeder.php
@@ -4,23 +4,20 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 
 class CharacterSeeder extends Seeder
 {
     public function run()
     {
-        $characters = [
-            ["name" => "Reimu Hakurei", "franchise_id" => 1],
-            ["name" => "Marisa Kirisame", "franchise_id" => 1],
-        ];
+        $characters = json_decode(File::get(database_path('data/characters.json')), true);
 
         foreach ($characters as $character) {
-            // Get franchise name
             $franchise = DB::table('franchises')->where('id', $character['franchise_id'])->first();
             $franchiseSlug = Str::slug($franchise->franchise_name, '_');
             $characterSlug = Str::slug($character['name'], '_');
-            
+
             DB::table('characters')->insert([
                 'character_name' => $character['name'],
                 'character_image' => "$franchiseSlug/$characterSlug.png",
