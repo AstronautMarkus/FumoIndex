af8c1278faa69c995c4060f20925f86955f2ab80
Marcos Reyes <astronautmarkus@gmail.com>
2025-04-21 16:10:45 -0400

    feat: implement getFranchises endpoint to retrieve franchise data

---
+++ FumoIndex/app/Http/Controllers/Franchises/FranchisesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Franchises;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Franchise;
+
+class FranchisesController extends Controller
+{
+    public function getFranchises(Request $request)
+    {
+        $franchises = Franchise::all()->map(function ($franchise) {
+            return [
+                'id' => $franchise->id,
+                'franchise_name' => $franchise->franchise_name,
+                'franchise_image' => url('/assets/franchises/' . $franchise->franchise_image),
+            ];
+        });
+
+        return response()->json($franchises);
+    }
+}
--- FumoIndex/routes/api.php
+++ FumoIndex/routes/api.php
@@ -4,7 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Characters\CharactersController;
 use App\Http\Controllers\FumoTypes\FumoTypesController;
+use App\Http\Controllers\Franchises\FranchisesController;
 
-Route::get('/characters/franchise/{franchiseId}', [CharactersController::class, 'getCharactersByFranchise'])->name('characters.franchise');
+Route::get('/characters/franchise/{franchiseId}', [CharactersController::class, 'getCharactersByFranchise']);
+Route::get('/franchises', [FranchisesController::class, 'getFranchises']);
 Route::get('/fumo-types', [FumoTypesController::class, 'getFumoTypesList']);
 Route::get('/fumo-types/{fumoTypeId}', [FumoTypesController::class, 'getFumoTypeById']);
