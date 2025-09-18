f0532afde97539dc1aea8b34b02eda3f2d048cde
Marcos Reyes <marcos@reyesandfriends.cl>
2025-09-18 15:24:32 -0300

    refactor: deleted icons and added font-bold in all options for styling.

--- FumoIndex/resources/views/components/header.blade.php
+++ FumoIndex/resources/views/components/header.blade.php
@@ -11,39 +11,33 @@
 
         <ul id="menu" class="hidden md:flex space-x-8 text-white text-lg flex-col md:flex-row md:items-center absolute md:static top-full left-0 w-full md:w-auto bg-primary md:bg-transparent">
             <li>
-                <a href="{{ route('home') }}" class="block px-4 py-2 hover:underline transition duration-300 ease-in-out hover:scale-105">
+                <a href="{{ route('home') }}" class="block px-4 py-2 hover:underline transition duration-300 ease-in-out hover:scale-105 font-bold">
                     Home
-                    <i class="fa-solid fa-home"></i>
                 </a>
             </li>
             <li>
-                <a href="#" class="block px-4 py-2 hover:underline transition duration-300 ease-in-out hover:scale-105">
+                <a href="#" class="block px-4 py-2 hover:underline transition duration-300 ease-in-out hover:scale-105 font-bold">
                     Fumo List
-                    <i class="fa-solid fa-list"></i>
                 </a>
             </li>
             <li>
-                <a href="{{ route('characters.list') }}" class="block px-4 py-2 hover:underline transition duration-300 ease-in-out hover:scale-105">
+                <a href="{{ route('characters.list') }}" class="block px-4 py-2 hover:underline transition duration-300 ease-in-out hover:scale-105 font-bold">
                     Character List
-                    <i class="fa-solid fa-users"></i>
                 </a>
             </li>
             <li>
-                <a href="{{ route('fumo_types') }}" class="block px-4 py-2 hover:underline transition duration-300 ease-in-out hover:scale-105">
+                <a href="{{ route('fumo_types') }}" class="block px-4 py-2 hover:underline transition duration-300 ease-in-out hover:scale-105 font-bold">
                     Fumo Types
-                    <i class="fa-solid fa-tags"></i>
                 </a>
             </li>
             <li>
-                <a href="{{ route('what_is_a_fumo') }}" class="block px-4 py-2 hover:underline transition duration-300 ease-in-out hover:scale-105">
+                <a href="{{ route('what_is_a_fumo') }}" class="block px-4 py-2 hover:underline transition duration-300 ease-in-out hover:scale-105 font-bold">
                     What is a Fumo
-                    <i class="fa-solid fa-question"></i>
                 </a>
             </li>
             <li>
-                <a href="{{ route('the_fumo_origins') }}" class="block px-4 py-2 hover:underline transition duration-300 ease-in-out hover:scale-105">
+                <a href="{{ route('the_fumo_origins') }}" class="block px-4 py-2 hover:underline transition duration-300 ease-in-out hover:scale-105 font-bold">
                     The Fumo Origins
-                    <i class="fa-solid fa-book"></i>
                 </a>
             </li>
         </ul>
