94456e7f4c83fb5356f062f39f952da95cc2fcbb
Marcos Reyes <astronautmarkus@gmail.com>
2026-01-02 02:23:06 -0300

    feat: Add Fumos link to admin header navigation

--- FumoIndex/resources/views/components/admin_header.blade.php
+++ FumoIndex/resources/views/components/admin_header.blade.php
@@ -10,6 +10,12 @@
         <div class="flex-1 flex justify-center">
             <ul id="menu" class="hidden md:flex space-x-8 text-white text-lg flex-col md:flex-row md:items-center absolute md:static top-full left-0 w-full md:w-auto bg-primary md:bg-transparent">
                 <li>
+                    <a href="{{ route('dashboard.fumos.index') }}" class="block px-4 py-2 hover:underline transition duration-300 ease-in-out hover:scale-105 font-bold">
+                        Fumos
+                    </a>
+                </li>
+
+                <li>
                     <a href="{{ route('dashboard.characters.index') }}" class="block px-4 py-2 hover:underline transition duration-300 ease-in-out hover:scale-105 font-bold">
                         Characters
                     </a>
